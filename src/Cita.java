52f7551a164c2e89cb28dd163e3975be896821ea
Fabian-Cruz93 <fabian.cruzangulo@gmail.com>
2023-06-14 11:55:26 -0700

    Actualizacion de codigo

---
+++ src/Cita.java
@@ -0,0 +1,35 @@
+public class Cita {
+    private int id;
+    private String fechaHora;
+    private String motivo;
+    private Doctor doctor;
+    private Paciente paciente;
+
+    public Cita(int id, String fechaHora, String motivo, Doctor doctor, Paciente paciente) {
+        this.id = id;
+        this.fechaHora = fechaHora;
+        this.motivo = motivo;
+        this.doctor = doctor;
+        this.paciente = paciente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+}
---
+++ src/Doctor.java
@@ -0,0 +1,23 @@
+public class Doctor {
+    private int id;
+    private String nombreCompleto;
+    private String especialidad;
+
+    public Doctor(int id, String nombreCompleto, String especialidad) {
+        this.id = id;
+        this.nombreCompleto = nombreCompleto;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+}
